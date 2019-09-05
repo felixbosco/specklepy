d09e28ce97372324e17c342a419ce1c6812f780d
Felix Bosco <bosco@mpia.de>
2019-09-05 15:47:55 +0200

    Create .gitignore file

--- holopy/tests/test_script_ssa_reconstruction.py
+++ holopy/tests/test_script_ssa_reconstruction.py
@@ -1,10 +1,13 @@
 import unittest
+import os
 
 
 class TestScriptSSAReconstrution(unittest.TestCase):
 
     def test_execute(self):
-        Aperture(8, 8, radius=4, data=self.test_data)
+        os.system('python spampy/scripts/extract_spectroastrometric_signal.py \
+                    -f data/test/spec2d_cN20170331S0216-pisco_GNIRS_2017Mar31T085412.181.fits \
+                    -d Pypeit-Gnirs')
 
 
 if __name__ == "__main__":
