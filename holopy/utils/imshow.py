09debfeeb3f69ac1b2f8339a22df1020a9575a68
Felix Bosco <43186689+felixbosco@users.noreply.github.com>
2019-09-10 15:47:58 +0200

    Update imshow.py

--- holopy/utils/imshow.py
+++ holopy/utils/imshow.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-def imshow(image):
-    plt.imshow(image)
+def imshow(image, norm=None):
+    plt.imshow(image, norm=norm)
     plt.colorbar(pad=0.0)
     plt.show()
     plt.close()
