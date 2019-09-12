2ba53192a7c461104edda48b640caf21d935a120
Felix Bosco <43186689+felixbosco@users.noreply.github.com>
2019-09-12 14:24:57 +0200

    Insert font defaults to pyplot_configs

---
+++ holopy/config/pyplot_defaults.py
@@ -0,0 +1,7 @@
+from matplotlib.pyplot import rc
+
+font = {'family' : 'monospace',
+        'weight' : 'bold',
+        'size'   : 'larger'}
+
+rc('font', **font)  # pass in the font dict as kwargs
