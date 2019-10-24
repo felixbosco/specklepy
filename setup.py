9ffbd6300747af2d449c22579a8b6f99de303509
Felix Bosco <bosco@mpia.de>
2019-10-24 14:41:29 +0200

    Incrementing version number to 0.0.1.dev1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='HoloPy',
-      version='0.0.1.dev0',
+      version='0.0.1.dev1',
       description='',
       long_description=long_description,
       long_description_content_type="text/markdown",
