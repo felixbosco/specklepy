561929e5d85c9f4725a39907dcfd91cc4b9c06ac
Felix Bosco <felixbosco@gmx.de>
2019-11-21 05:40:23 +0100

    Increment version number for clean up of the holography algorithm and sub-pixel alignment in PSF extraction

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='HoloPy',
-      version='0.0.2',
+      version='0.0.3',
       description='',
       long_description=long_description,
       long_description_content_type="text/markdown",
