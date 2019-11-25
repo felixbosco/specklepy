9d781cbb3cb7bb17c2b8b565b850b8a6b8697f38
Felix Bosco <felixbosco@gmx.de>
2019-11-25 07:44:13 +0100

    Make Apdizer accept shapes that are rectangular

--- holopy/core/apodizer.py
+++ holopy/core/apodizer.py
@@ -6,10 +6,13 @@ from holopy.utils.transferfunctions import otf
 
 class Apodizer(object):
 
-    def __init__(self, function, size, **kwargs):
+    def __init__(self, function, shape, **kwargs):
 
-        self.size = size
-        self.center = (self.size + 1) / 2
+        if not isinstance(shape, tuple):
+            if isinstance(shape, int):
+                shape = (shape, shape)
+        self.shape = shape
+        self.center = ((self.shape[0] + 1) / 2, (self.shape[1] + 1) / 2)
 
         if function == 'Gaussian':
             if 'radius' in kwargs:
@@ -17,20 +20,20 @@ class Apodizer(object):
                 kwargs['x_stddev'] = kwargs['radius']
                 kwargs['y_stddev'] = kwargs['radius']
                 del kwargs['radius']
-            self.model = Gaussian2D(x_mean=self.center, y_mean=self.center, **kwargs)
+            self.model = Gaussian2D(x_mean=self.center[0], y_mean=self.center[1], **kwargs)
         elif function == 'Airy':
-            self.model = AiryDisk2D(x_0=self.center, y_0=self.center, **kwargs)
+            self.model = AiryDisk2D(x_0=self.center[0], y_0=self.center[1], **kwargs)
         else:
             raise ValueError("Function value <{}> for Apodizer class is not recognized!".format(function))
 
 
     def __call__(self):
-        y, x = np.mgrid[0:self.size, 0:self.size]
+        y, x = np.mgrid[0:self.shape[0], 0:self.shape[1]]
         return self.model(x, y)
 
 
     def apodize(self, object):
-        y, x = np.mgrid[0:self.size, 0:self.size]
+        y, x = np.mgrid[0:self.shape[0], 0:self.shape[1]]
         apodization_psf = self.model(x, y)
         apodization_otf = otf(apodization_psf)
         return np.multiply(object, apodization_otf)
