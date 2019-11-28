7037362fea4a0daff23482654bc897d67e02e48a
Felix Bosco <felixbosco@gmx.de>
2019-11-28 20:17:02 +0100

    Derive function from apodizer class and put into a new core module

---
+++ holopy/core/apodization.py
@@ -0,0 +1,55 @@
+import numpy as np
+from astropy.modeling.models import Gaussian2D
+from astropy.modeling.models import AiryDisk2D
+
+from holopy.utils.transferfunctions import otf
+from holopy.logging import logging
+
+
+
+def apodize(object, function='gaussian', **kwargs):
+    """Apodize an object with a given model function.
+
+    Long description...
+
+    Args:
+        object (np.ndarray): Object to apodize.
+        function (str, optional): Function to initialize the apodization model
+            along. Models are astropy.modelling.models Gaussian2D or AiryDisk2D.
+            Default is 'gaussian'.
+        kwargs (optional): kwargs are passed to the model function.
+    """
+
+
+    # Assert object type and shape
+    if not isinstance(object, np.ndarray) and object.ndim != 2:
+        raise ValueError("object must be a 2-dimensional np.ndarray!")
+    if object.shape[0] != object.shape[1]:
+        logging.warn("holopy.core.apodization.apodize received a non quadratic input image. This may cause some unpredictable results!")
+
+
+
+    # Interprete function input and initialize model function
+    _function = function.lower()
+    center = ((object.shape[0] + 1) / 2, (object.shape[1] + 1) / 2)
+
+    if 'gauss' in _function:
+        if 'radius' in kwargs:
+            # Copying the radius keyword argument into the proper Gaussian2D keywords
+            kwargs['x_stddev'] = kwargs['radius']
+            kwargs['y_stddev'] = kwargs['radius']
+            del kwargs['radius']
+        model = Gaussian2D(x_mean=center[0], y_mean=center[1], **kwargs)
+
+    elif 'airy' in _function:
+        model = AiryDisk2D(x_0=center[0], y_0=center[1], **kwargs)
+
+    else:
+        raise ValueError("holopy.core.apodization.apodize received unknown value for function '{}'!".format(function))
+
+
+    # Evaluate apodization function on object image grid and multiply on object
+    y, x = np.mgrid[0:object.shape[0], 0:object.shape[1]]
+    apodization_psf = model(x, y)
+    apodization_otf = otf(apodization_psf)
+    return np.multiply(object, apodization_otf)
---
+++ holopy/tests/test_apodization.py
@@ -0,0 +1,25 @@
+import unittest
+import numpy as np
+from holopy.core.apodization import apodize
+from holopy.utils.plot import imshow
+
+
+class TestApodization(unittest.TestCase):
+
+    def setUp(self):
+        self.object = np.zeros((256, 256))
+        self.object[128, 128] = 10
+
+    def test_call(self):
+        apodize(self.object, 'Gaussian', radius=16)
+        apodize(self.object, 'Airy', radius=16)
+        imshow(np.abs(apodize(self.object, 'Gaussian', radius=20)), norm='log')
+
+    def test_errors(self):
+
+
+        with self.assertRaises(ValueError):
+            apodize(self.object, 'Nonsense')
+
+if __name__ == "__main__":
+    unittest.main()
