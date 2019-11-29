b3a9c880ba3aa7f5a6aadbf4b7263b9c038f6e92
Felix Bosco <felixbosco@gmx.de>
2019-11-29 11:11:47 +0100

    Start renaming the package to specklepy

---
+++ specklepy/deprecated/plotting.py
@@ -0,0 +1,9 @@
+from matplotlib.pyplot import rc
+
+font = {
+    'family' : 'serif',
+    #'weight' : 'bold',
+    #'size'   : 'larger'
+    }
+
+rc('font', **font)  # pass in the font dict as kwargs
