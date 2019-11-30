52be91c4d099cf8b8b7fc8f5eee6d001b30dd421
Felix Bosco <felixbosco@gmx.de>
2019-11-30 21:02:36 +0100

    Implement desaturate_color function, which takes a color and returns a list of desaturated instances

--- specklepy/tests/test_plot.py
+++ specklepy/tests/test_plot.py
@@ -4,7 +4,7 @@ from matplotlib.colors import LogNorm
 
 from specklepy.core.aperture import Aperture
 from specklepy.utils.transferfunctions import psf, otf, mtf, powerspec
-from specklepy.utils.plot import imshow, plot_powerspec1d
+from specklepy.utils import plot
 
 
 class TestTransferFunctions(unittest.TestCase):
@@ -18,8 +18,13 @@ class TestTransferFunctions(unittest.TestCase):
 
     def test_plot_powerspec1d(self):
         psf_image = psf(self.aperture)
-        imshow(psf_image, title="PSF", norm=LogNorm())
-        plot_powerspec1d(psf_image)
+        plot.imshow(psf_image, title="PSF", norm=LogNorm())
+        plot.plot_powerspec1d(psf_image)
+
+    def test_desaturate_color(self):
+        plot.desaturate_color('tab:blue', ncolors=3)
+        plot.desaturate_color('#00FF00', ncolors=3)
+        plot.desaturate_color((0, 1, 0), ncolors=3)
 
 if __name__ == "__main__":
     unittest.main()
--- specklepy/utils/plot.py
+++ specklepy/utils/plot.py
@@ -1,29 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-# from specklepy.config import plotting
+import matplotlib.colors as clrs
+
+from specklepy.logging import logging
 from specklepy.utils import transferfunctions as tf
 
-from matplotlib.pyplot import rc
 
+
+# Define plotting defaults
 font = {
     'family' : 'serif',
     #'weight' : 'bold',
     #'size'   : 'larger'
     }
 
-rc('font', **font)  # pass in the font dict as kwargs
+plt.rc('font', **font)  # pass in the font dict as kwargs
+
 
 
 def imshow(image, title=None, norm=None):
     if norm == 'log':
-        norm = LogNorm()
+        norm = clrs.LogNorm()
     plt.figure()
     plt.imshow(image, norm=norm)
     plt.title(title)
     plt.colorbar(pad=0.0)
     plt.show()
     plt.close()
+
+
+
+def plot_simple(xdata, ydata, title=None, xlabel=None, ylabel=None):
+    plt.plot(xdata, ydata)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+    plt.close()
+
 
 
 def plot_powerspec1d(image, title=None, average=True, pixel_scale=None):
@@ -43,10 +57,54 @@ def plot_powerspec1d(image, title=None, average=True, pixel_scale=None):
     plt.close()
 
 
-def plot_simple(xdata, ydata, title=None, xlabel=None, ylabel=None):
-    plt.plot(xdata, ydata)
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.show()
-    plt.close()
+
+def desaturate_color(color, ncolors=1, saturation_values=None, saturation_min=0.1):
+    """Desaturates a color and returns a list of desaturated colors.
+
+    Args:
+        color (str or ...):
+        ncolors (int): Number of returned colors.
+        saturation_values (None or list, dtype=float):
+        saturation_min (float): Minimum value of saturation.
+
+    Returns:
+        colors (list): List of RGB represnations of colors with length ncolors.
+    """
+
+    # Input parameters
+    if isinstance(color, str):
+        rgb_color = clrs.to_rgb(color)
+        hsv_color = clrs.rgb_to_hsv(rgb_color)
+    elif isinstance(color, tuple):
+        logging.info("Interpreting color tuple () as RGB values.")
+        hsv_color = clrs.rgb_to_hsv(color)
+    else:
+        raise TypeError("The function desaturate_color received color argument \
+                        of type {}, but needs to be str!".format(type(color)))
+
+    if not isinstance(ncolors, int):
+        raise TypeError("The function desaturate_color received ncolor argument\
+                        of type {}, but needs to be int!".format(type(ncolors)))
+
+    if not isinstance(saturation_min, float):
+        raise TypeError("The function desaturate_color received saturation_min\
+                        argument of type {}, but needs to be float!".format(type(saturation_min)))
+
+    if saturation_values is None:
+        saturation_values = np.linspace(hsv_color[1], saturation_min, num=ncolors)
+    elif isinstance(saturation_values, list):
+        pass # list is correct, nothing to adapt
+    elif isinstance(saturation_values, float):
+        saturation_values = list(saturation_values)
+    else:
+        raise TypeError("The function desaturate_color received \
+                        saturation_values argument of type {}, but needs to be list!".format(type(saturation_values)))
+
+
+    # Create list of colors with varied saturation values
+    colors = []
+    for saturation_value in saturation_values:
+        color = clrs.to_rgb((hsv_color[0], saturation_value, hsv_color[2]))
+        colors.append(color)
+
+    return colors
