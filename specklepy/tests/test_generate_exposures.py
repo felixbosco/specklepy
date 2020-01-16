4c77dfb768901fc123f2cb2c205fb609d1354262
Felix Bosco <bosco@mpia.de>
2020-01-16 17:38:47 +0100

    Add example par files for synthetic obsrevations and add corresponding test

--- specklepy/tests/test_generate_exposures.py
+++ specklepy/tests/test_generate_exposures.py
@@ -2,7 +2,7 @@ import unittest
 import os
 
 
-class TestGenerateExposure(unittest.TestCase):
+class TestGenerateExposures(unittest.TestCase):
 
     def setUp(self):
         self.parameter_file = 'data/test/test_synthetic_exposures.par'
@@ -10,12 +10,13 @@ class TestGenerateExposure(unittest.TestCase):
     def test_call(self):
         os.system('python specklepy/scripts/generate_exposures.py -p {}'.format(self.parameter_file))
 
-    # def test_study_data(self):
-    #     path = '../synthetic_observations/par/'
-    #     for mode in ['airy']:#, 'sky', 'noao', 'glao']:
-    #         for time in ['200', '600', '1200']:
-    #             parameter_file = "{}{}_{}ms.par".format(path, mode, time)
-    #             os.system('python specklepy/scripts/generate_exposures.py -p {}'.format(parameter_file))
+    def test_study_data(self):
+        path = 'data/example/synthetic/'
+        for mode in ['airy', 'dark', 'sky', 'noao', 'glao']:
+            for time in ['200', '600', '1200']:
+                parameter_file = "{}{}_{}ms.par".format(path, mode, time)
+                os.system('python specklepy/scripts/generate_exposures.py -p {}'.format(parameter_file))
+        os.system('python specklepy/scripts/generate_exposures.py -p {}flat.par'.format(path))
 
 
 
