2554ef60d7908fa309af7263d8e2bfd49176b652
Felix Bosco <bosco@mpia.de>
2020-01-23 16:47:40 +0100

    Change to new parameter file for the holographic reconstruction of multiple cubes

--- specklepy/tests/test_holographic_reconstruction_multiple_files.py
+++ specklepy/tests/test_holographic_reconstruction_multiple_files.py
@@ -5,8 +5,7 @@ import os
 class TestHolographicReconstruction(unittest.TestCase):
 
     def test_execute(self):
-        # os.system('python specklepy/scripts/holographic_reconstruction.py -p {}/../holography/holography.ini'.format(os.getcwd()))
-        os.system('python specklepy/scripts/holographic_reconstruction.py -p /home/bosco/Documents/sowat/synthetic_observations/holography.par')
+        os.system('python specklepy/scripts/holographic_reconstruction.py -p specklepy/tests/files/test_holography.par')
 
 
 
