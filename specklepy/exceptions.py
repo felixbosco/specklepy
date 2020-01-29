a2c2807c3163ad2e73d2d53fcd9a7f99c9eae02f
Felix Bosco <bosco@mpia.de>
2020-01-29 16:17:07 +0100

    Change formatting

--- specklepy/exceptions.py
+++ specklepy/exceptions.py
@@ -2,7 +2,7 @@ class SpecklepyValueError(ValueError):
 
     def __init__(self, object, argname, argvalue, expected, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        message = "{} received argument {}={}, but must be {}!".format(object, argname, argvalue, expected)
+        message = "{!r} received argument {}={}, but must be {!r}!".format(object, argname, argvalue, expected)
         raise ValueError(message, *args, **kwargs)
 
 
@@ -10,5 +10,5 @@ class SpecklepyTypeError(TypeError):
 
     def __init__(self, object, argname, argtype, expected, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        message = "{} received argument {} of type {}, but must be {} type!".format(object, argname, argtype, expected)
+        message = "{!r} received argument {!r} of type {!r}, but must be {!r} type!".format(object, argname, argtype, expected)
         raise TypeError(message, *args, **kwargs)
