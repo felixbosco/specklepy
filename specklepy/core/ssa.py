5fbae05a24613513e5eb4f569549166810748a85
Felix Bosco <43186689+felixbosco@users.noreply.github.com>
2020-02-10 17:23:16 +0100

    Update doc strings

--- specklepy/core/ssa.py
+++ specklepy/core/ssa.py
@@ -12,25 +12,48 @@ from specklepy.core import alignment
 def ssa(files, mode='same', reference_file=0, outfile=None, tmp_dir=None, lazy_mode=True, debug=False, **kwargs):
     """Compute the SSA reconstruction of a list of files.
 
-    Long description...
+    The simple shift-and-add (SSA) algorithm makes use of the structure of
+    typical speckle patterns, i.e. short-exposure point-spread functions (PSFs).
+    These show multiple peaks resembling the diffraction-limited PSF of coherent
+    fractions within the telescope aperture. Under good conditions or on small
+    telescopes, there is typically one largest coherent atmospheric cell and
+    therefore, speckle PSFs typically show one major intensity peak. The
+    algorithm makes use of this fact and identifies the emission peak in a given
+    observation frame, assuming that this always belongs to the same star, and
+    aligns all frames on the coordinate of the emission peak.
+
+    See Bates & Cady (1980) for references.
 
     Args:
         files (list):
+            List of complete paths to the fits files that shall be considered
+            for the SSA reconstruction.
         mode (str):
+            Name of the reconstruction mode: In 'same' mode, the reconstruction
+            covers the same field of view of the reference file. In 'full' mode,
+            every patch of the sky that is covered by at least one frame will be
+            contained in the final reconstruction.
         reference_file (str, int, optional):
             Path to a reference file or index of the file in files, relative to
             which the shifts are computed. See
             specklepy.core.aligment.get_shifts for details. Default is 0.
         outfile (specklepy.io.recfile, optional): Object to write the result to,
             if provided.
+        tmp_dir (str, optional):
+            Path of a directory in which the temporary results are stored in.
+        lazy_mode (bool, optional):
+            Set to False, to enforce the alignment of a single file with respect
+            to the reference file. Default is True.
         debug (bool, optional): Set to True to inspect intermediate results.
             Default is False.
 
     Returns:
-        reconstruction (np.ndarray): The image reconstruction.
+        reconstruction (np.ndarray):
+            The image reconstruction. The size depends on the mode argument.
     """
 
     logging.info("Starting SSA reconstruction...")
+    # Check parameters
     if not isinstance(files, list):
         files = [files]
 
@@ -44,7 +67,6 @@ def ssa(files, mode='same', reference_file=0, outfile=None, tmp_dir=None, lazy_m
             outfile = RECfile(files=files, filename=outfile, cards={"RECONSTRUCTION": "SSA"})
         else:
             raise TypeError("specklepy.core.ssa.ssa received outfile argument of wrong type <{}>!".format(type(outfile)))
-
 
     # Do not align just a single file
     if lazy_mode and len(files) == 1:
@@ -89,9 +111,10 @@ def coadd_frames(cube):
 
     Args:
         cube (np.ndarray, ndim=3):
-
+            Data cube which is integrated along the zero-th axis.
     Returns:
-        coadded (np.ndarray, ndim=2): SSA-integrated frames of the input cube.
+        coadded (np.ndarray, ndim=2):
+            SSA-integrated frames of the input cube.
     """
 
     if not isinstance(cube, np.ndarray):
