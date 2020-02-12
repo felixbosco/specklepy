650eea612caee2eb5d5f1ae0f7fb6250d072cf37
Felix Bosco <bosco@mpia.de>
2020-02-12 16:38:24 +0100

    Update paths

--- specklepy/tests/test_psffile.py
+++ specklepy/tests/test_psffile.py
@@ -1,20 +1,27 @@
 import unittest
+import os
 from numpy.random import rand
-from specklepy.io.filemanager import FileManager
+
 from specklepy.io.psffile import PSFfile
+
 
 
 class TestPSFfile(unittest.TestCase):
 
     def setUp(self):
         self.frame_shape = (25, 25)
+        self.path = "specklepy/tests/files/"
+        self.file = os.path.join(self.path, 'example_cube.fits')
+        self.tmpdir = os.path.join(self.path, 'tmp/')
 
     def test_init(self):
-        PSFfile(inFile="data/test/example_cube.fits", outDir="data/test/tmp/", frame_shape=self.frame_shape, cards={"RECONSTRUCTION": "Test"}, header_prefix="HIERARCH SPECKLEPY ")
+        PSFfile(inFile=self.file, outDir=self.tmpdir, frame_shape=self.frame_shape, cards={"RECONSTRUCTION": "Test"}, header_prefix="HIERARCH SPECKLEPY ")
 
     def test_set_data(self):
-        psf_file = PSFfile(inFile="data/test/example_cube.fits", outDir="data/test/tmp/", frame_shape=self.frame_shape, cards={"RECONSTRUCTION": "Test"}, header_prefix="HIERARCH SPECKLEPY ")
+        psf_file = PSFfile(inFile=self.file, outDir=self.tmpdir, frame_shape=self.frame_shape, cards={"RECONSTRUCTION": "Test"}, header_prefix="HIERARCH SPECKLEPY ")
         psf_file.update_frame(3, rand(*self.frame_shape))
+
+
 
 if __name__ == "__main__":
     unittest.main()
