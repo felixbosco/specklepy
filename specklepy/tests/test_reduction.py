8d25aaf0880cc8632054ed656df1c8e2e60dafbf
Felix Bosco <bosco@mpia.de>
2020-02-20 16:29:49 +0100

    Remove flag for the now positional argument parameter file

--- specklepy/tests/test_reduction.py
+++ specklepy/tests/test_reduction.py
@@ -9,7 +9,7 @@ class TestReduction(unittest.TestCase):
         self.parameter_file = 'specklepy/tests/files/test_reduction.par'
 
     def test_call(self):
-        os.system('python specklepy/scripts/reduction.py -p {}'.format(self.parameter_file))
+        os.system('python specklepy/scripts/reduction.py {}'.format(self.parameter_file))
 
 
 
