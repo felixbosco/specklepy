2c160a6d3353c090f682b85c19dcc011f76e120f
Felix Bosco <bosco@mpia.de>
2020-02-20 16:09:34 +0100

    Change parent file to a small file for testing

--- specklepy/tests/test_reductionfile.py
+++ specklepy/tests/test_reductionfile.py
@@ -9,7 +9,7 @@ class TestReductionFile(unittest.TestCase):
 
     def setUp(self):
         self.path = "specklepy/tests/files/"
-        self.parentFile = os.path.join(self.path, "synthetic/noao_200ms_1.fits")
+        self.parentFile = os.path.join(self.path, "reduction/MasterFlat.fits")
 
     def test_init(self):
         ReductionFile(self.parentFile, prefix='rtest_', path=os.path.join(self.path, 'reduction/'), reduction='bkg')
