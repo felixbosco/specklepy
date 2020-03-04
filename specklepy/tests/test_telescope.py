c34fc077b683b1e40d07083693ad91f7c8973990
Felix Bosco <bosco@mpia.de>
2020-03-04 13:06:40 +0100

    Amend indentation

--- specklepy/tests/test_telescope.py
+++ specklepy/tests/test_telescope.py
@@ -19,11 +19,11 @@ class TestTelescope(unittest.TestCase):
         telescope_gaussian_psf = Telescope(8.2*u.m, psf_source='Gaussian', radius=0.1645*u.arcsec, psf_resolution=0.0106*u.arcsec)
         assert np.abs(np.sum(telescope_gaussian_psf.psf) - 1.0) < 1e-6
         if self.visual > 0:
-        	imshow(telescope_gaussian_psf.psf, title="Test 'Gaussian' model")
+            imshow(telescope_gaussian_psf.psf, title="Test 'Gaussian' model")
 
         telescope_airydisk_psf = Telescope(8.2*u.m, psf_source='AiryDisk', radius=0.4777*u.arcsec, psf_resolution=0.0106*u.arcsec)
         if self.visual > 0:
-        	imshow(telescope_airydisk_psf.psf, title="Test 'AiryDisk' model")
+            imshow(telescope_airydisk_psf.psf, title="Test 'AiryDisk' model")
 
     # def test_init_static(self):
     #     telescope_static_psf = Telescope(8.0*u.m, central_obscuration=0.14, name="VLT Unit Telescope", psf_source=self.scao_long_exposure_psf_file )
