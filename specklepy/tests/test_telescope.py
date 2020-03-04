7be2e3f85b6a8f637494d0a554dfcdd11cb10597
Felix Bosco <43186689+felixbosco@users.noreply.github.com>
2020-03-04 11:28:48 +0100

    Split init test into multiple smaller tests

--- specklepy/tests/test_telescope.py
+++ specklepy/tests/test_telescope.py
@@ -25,6 +25,21 @@ class TestTelescope(unittest.TestCase):
         if self.visual > 0:
         	imshow(telescope_airydisk_psf.psf, title="Test 'AiryDisk' model")
 
+    # def test_init_static(self):
+    #     telescope_static_psf = Telescope(8.0*u.m, central_obscuration=0.14, name="VLT Unit Telescope", psf_source=self.scao_long_exposure_psf_file )
+    #     assert np.abs(np.sum(telescope_static_psf.psf) - 1.0) < 1e-6
+    #
+    # def test_init_gaussianmodel(self):
+    #     telescope_gaussian_psf = Telescope(8.2*u.m, psf_source='Gaussian', radius=0.1645*u.arcsec, psf_resolution=0.0106*u.arcsec)
+    #     assert np.abs(np.sum(telescope_gaussian_psf.psf) - 1.0) < 1e-6
+    #     if self.visual > 0:
+    #     	imshow(telescope_gaussian_psf.psf, title="Test 'Gaussian' model")
+    #
+    # def test_init_airymodel(self):
+    #     telescope_airydisk_psf = Telescope(8.2*u.m, psf_source='AiryDisk', radius=0.4777*u.arcsec, psf_resolution=0.0106*u.arcsec)
+    #     if self.visual > 0:
+    #     	imshow(telescope_airydisk_psf.psf, title="Test 'AiryDisk' model")
+
     def test_call(self):
         pass
 
