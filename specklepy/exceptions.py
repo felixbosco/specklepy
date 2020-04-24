a52d74dfdc7b483059c5881c1969562ca69bd863
Felix Bosco <felixbosco@gmx.de>
2020-04-24 13:09:24 +0200

    Add documentation

--- specklepy/exceptions.py
+++ specklepy/exceptions.py
@@ -2,11 +2,11 @@ class SpecklepyValueError(ValueError):
 
     """ValueError with message that is composed in a default format."""
 
-    def __init__(self, object, argname, argvalue, expected, *args, **kwargs):
+    def __init__(self, objname, argname, argvalue, expected, *args, **kwargs):
         """Raise a ValueError with formatted message.
 
         Args:
-            object (str):
+            objname (str):
                 Name of the object/ function/ etc. that is raising the error.
             argname (str):
                 Name of the argument that received input of value.
@@ -19,7 +19,7 @@ class SpecklepyValueError(ValueError):
             **kwargs:
                 Passed to ValueError.
         """
-        message = "{!r} received argument {}={}, but must be {!r}!".format(object, argname, argvalue, expected)
+        message = "{!r} received argument {}={}, but must be {!r}!".format(objname, argname, argvalue, expected)
         # super().__init__(message, *args, **kwargs)
         raise ValueError(message, *args, **kwargs)
 
@@ -28,11 +28,11 @@ class SpecklepyTypeError(TypeError):
 
     """TypeError with message that is composed in a default format."""
 
-    def __init__(self, object, argname, argtype, expected, *args, **kwargs):
+    def __init__(self, objname, argname, argtype, expected, *args, **kwargs):
         """Raise a TypeError with formatted message.
 
         Args:
-            object (str):
+            objname (str):
                 Name of the object/ function/ etc. that is raising the error.
             argname (str):
                 Name of the argument that received input of value.
@@ -40,11 +40,11 @@ class SpecklepyTypeError(TypeError):
                 Type that was received.
             expected (str):
                 Expected type for argument argname.
-            *args: 
+            *args:
                 Passed to TypeError.
             **kwargs:
                 Passed to TypeError.
         """
-        message = "{!r} received argument {!r} of type {!r}, but must be {!r} type!".format(object, argname, argtype, expected)
+        message = "{!r} received argument {!r} of type {!r}, but must be {!r} type!".format(objname, argname, argtype, expected)
         # super().__init__(message, *args, **kwargs)
         raise TypeError(message, *args, **kwargs)
