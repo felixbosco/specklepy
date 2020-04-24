68a5548e1bdd4b089e762eab1d6651dda7579b1f
Felix Bosco <felixbosco@gmx.de>
2020-04-24 13:58:35 +0200

    Implement and test computation of time differences

--- specklepy/tests/test_combine.py
+++ specklepy/tests/test_combine.py
@@ -1,9 +1,8 @@
 import unittest
 import numpy as np
 
-from specklepy.utils.combine import weighted_mean
+from specklepy.utils.combine import weighted_mean, time_difference
 from specklepy.utils.plot import imshow
-
 
 
 class TestCombine(unittest.TestCase):
@@ -17,13 +16,19 @@ class TestCombine(unittest.TestCase):
         self.data[3] *= 5
         self.vars = np.random.rand(*self.shape) * 0.5
 
-
     def test_weighted_combin(self):
         mean, weights = weighted_mean(self.data, axis=0)
         mean, weights = weighted_mean(self.data, axis=0, vars=self.vars)
         imshow(mean)
         imshow(weights)
 
+    def test_time_difference(self):
+        t0 = '2020-04-23 13:02:31.397899'
+        self.assertTrue(time_difference(t0, t0) == 0)
+        times = ['2018-11-14 16:51:37.070953', '2020-04-17 21:49:47.877587',
+                 '2020-04-23 13:02:31.397899', '2020-04-23 13:14:11.297996']
+        deltas = time_difference(t0, times)
+        self.assertTrue(type(deltas) is np.ndarray)
 
 
 if __name__ == "__main__":
--- specklepy/utils/combine.py
+++ specklepy/utils/combine.py
@@ -1,4 +1,9 @@
 import numpy as np
+from datetime import datetime
+from dateutil import parser
+
+from specklepy.exceptions import SpecklepyTypeError
+
 
 def weighted_mean(data, axis=None, weights=None, vars=None):
     """Combine data frames considering weights and variances.
@@ -23,6 +28,47 @@ def weighted_mean(data, axis=None, weights=None, vars=None):
 
     mean, weights = np.average(data, axis=axis, weights=weights, returned=True)
 
-    weights = np.ma.masked_values(weights, 0.0) # Avoid division by zero
+    weights = np.ma.masked_values(weights, 0.0)  # Avoid division by zero
 
     return mean, np.divide(1, weights)
+
+
+def time_difference(t0, times):
+    """Compute time difference(s) from one time to others.
+
+    Args:
+        t0 (str, datetime):
+            Time stamp whose difference to times is computed.
+        times (str, datetime, list):
+            Times to which the time difference is computed.
+
+    Returns:
+        timedeltas (float, np.array):
+            Time difference(s) between from t0 to times (in seconds).
+    """
+
+    # Type check
+    if isinstance(t0, str):
+        t0 = parser.parse(t0)
+    elif isinstance(t0, datetime):
+        pass
+    else:
+        raise SpecklepyTypeError('distance', 'obj', type(t0), 'str or datetime')
+
+    if isinstance(times, str):
+        times = parser.parse(times)
+    elif isinstance(times, (datetime, list)):
+        pass
+    else:
+        raise SpecklepyTypeError('distance', 'objs', type(times), 'str, datetime, or list')
+
+    # Compute time deltas in units of seconds
+    if isinstance(times, list):
+        timedeltas = np.empty((len(times)))
+        for i, o in enumerate(times):
+            if isinstance(o, str):
+                o = parser.parse(o)
+            timedeltas[i] = (o - t0).total_seconds()
+        return timedeltas
+    else:
+        return (times - t0).total_seconds()
