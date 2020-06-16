0a1323facec0e3f7d4f8eae6ce7565b13efa8238
Felix Bosco <felixbosco@gmx.de>
2020-06-16 18:42:04 +0200

    Refactor code

--- specklepy/io/reconstructionfile.py
+++ specklepy/io/reconstructionfile.py
@@ -1,9 +1,6 @@
-import numpy as np
 from os import path
 from astropy.io import fits
-from datetime import datetime
 
-from specklepy.logging import logger
 from specklepy.io.outfile import Outfile
 
 
@@ -22,4 +19,5 @@ class ReconstructionFile(Outfile):
         hdr_input = fits.getheader(files[0])
         shape = (hdr_input['NAXIS1'], hdr_input['NAXIS2'])
 
-        super().__init__(filename=filename, shape=shape, extensions=None, cards=cards, timestamp=False, header_card_prefix=header_card_prefix)
+        super().__init__(filename=filename, shape=shape, extensions=None, cards=cards, timestamp=False,
+                         header_card_prefix=header_card_prefix)
