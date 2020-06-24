b8365186cf45b9543244de110cc27cb266be81d4
Felix Bosco <felixbosco@gmx.de>
2020-06-24 17:38:42 +0200

    Disable GUI while developing

--- specklepy/io/argparser.py
+++ specklepy/io/argparser.py
@@ -20,7 +20,7 @@ class GeneralArgParser(object):
                         'algorithms are implemented for image reconstruction: the SSA and '
                         'Holography algorithm.',
             epilog="Execute 'specklepy <command> -h' for further information on the commands.")
-        self.parser.add_argument('--gui', action='store_true', help='Start the graphical user interface (GUI).')
+        # self.parser.add_argument('--gui', action='store_true', help='Start the graphical user interface (GUI).')
         self.parser.add_argument('-d', '--debug', action='store_true', help='Show debugging information.')
         subparsers = self.parser.add_subparsers(help='Available commands in Specklepy:')
 
