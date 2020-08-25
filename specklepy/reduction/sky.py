2d289864002c1d6545f7a8aa4d87314d0a4afb90
Felix Bosco <bosco@mpia.de>
2020-08-25 18:58:28 +0200

    Comment out deprecated code

--- specklepy/reduction/sky.py
+++ specklepy/reduction/sky.py
@@ -303,37 +303,37 @@ def subtract_scalar_background(files, params, prefix=None, debug=False):
     logger.info("Scalar background subtraction complete!")
 
 
-def subtract(params, mode='constant', debug=False):
-    logger.info("Subtracting sky from files\n\t{}".format(params.scienceFiles))
-    logger.info("Sky files are\n\t{}".format(params.skyFiles))
-
-    if isinstance(params.skyFiles, str):
-        skyFiles = [params.skyFiles]
-    if len(skyFiles) > 1:
-        raise ValueError("sky.subtract does not handle more than one file yet!")
-
-    skyFile = skyFiles[0]
-    if mode == 'constant':
-        median = np.median(fits.getdata(skyFile))
-    elif mode == 'image':
-        median = np.median(fits.getdata(skyFile), axis=0)
-        imshow(median)
-
-    for scienceFile in params.scienceFiles:
-        # Create a copy
-        skySubtractedScienceFile = os.path.basename(scienceFile)
-        skySubtractedScienceFile = os.path.join(params.tmpDir, "s{}".format(skySubtractedScienceFile))
-        logger.info("Subtrcting sky and saving tmp file:\n\t{}".format(skySubtractedScienceFile))
-        os.system("cp {} {}".format(scienceFile, skySubtractedScienceFile))
-
-        with fits.open(skySubtractedScienceFile, mode='update') as hdulist:
-            for frame_index, frame in enumerate(hdulist[0].data):
-                hdulist[0].data[frame_index] -= median
-                hdulist.flush()
+# def subtract(params, mode='constant', debug=False):
+#     logger.info("Subtracting sky from files\n\t{}".format(params.scienceFiles))
+#     logger.info("Sky files are\n\t{}".format(params.skyFiles))
+#
+#     if isinstance(params.skyFiles, str):
+#         skyFiles = [params.skyFiles]
+#     if len(skyFiles) > 1:
+#         raise ValueError("sky.subtract does not handle more than one file yet!")
+#
+#     skyFile = skyFiles[0]
+#     if mode == 'constant':
+#         median = np.median(fits.getdata(skyFile))
+#     elif mode == 'image':
+#         median = np.median(fits.getdata(skyFile), axis=0)
+#         imshow(median)
+#
+#     for scienceFile in params.scienceFiles:
+#         # Create a copy
+#         skySubtractedScienceFile = os.path.basename(scienceFile)
+#         skySubtractedScienceFile = os.path.join(params.tmpDir, "s{}".format(skySubtractedScienceFile))
+#         logger.info("Subtrcting sky and saving tmp file:\n\t{}".format(skySubtractedScienceFile))
+#         os.system("cp {} {}".format(scienceFile, skySubtractedScienceFile))
+#
+#         with fits.open(skySubtractedScienceFile, mode='update') as hdulist:
+#             for frame_index, frame in enumerate(hdulist[0].data):
+#                 hdulist[0].data[frame_index] -= median
+#                 hdulist.flush()
 
 
 def estimate_sky_background(data, method='scalar', mask_sources=True, path=None):
-    
+
     """Estimate a scalar or image sky background with uncertainties.
 
     Args:
