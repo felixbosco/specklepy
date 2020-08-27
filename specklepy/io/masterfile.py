f5397b21e98da2abe56c167c4df2b4f74b26df53
Felix Bosco <bosco@mpia.de>
2020-08-27 13:30:34 +0200

    Propagate file path into MasterFile

--- specklepy/io/masterfile.py
+++ specklepy/io/masterfile.py
@@ -1,4 +1,5 @@
-from os import path
+import os
+
 from astropy.io import fits
 
 from specklepy.io.outfile import Outfile
@@ -6,21 +7,31 @@ from specklepy.io.outfile import Outfile
 
 class MasterFile(Outfile):
 
-    def __init__(self, filename, files, shape=None, cards=None, out_dir=None, header_card_prefix="HIERARCH SPECKLEPY"):
+    def __init__(self, filename, files, shape=None, cards=None, in_dir=None, out_dir=None,
+                 header_card_prefix="HIERARCH SPECKLEPY"):
 
+        # Apply fall back value
         if cards is None:
             cards = {}
 
         # Add list of files to header
         for index, file in enumerate(files):
-            cards["FILE {}".format(index)] = path.basename(file)
+            cards["FILE {}".format(index)] = os.path.basename(file)
 
+        # Derive shape from FITS header
         if shape is None:
-            hdr_input = fits.getheader(files[0])
-            try:
+            # Read header information
+            if in_dir:
+                hdr_input = fits.getheader(os.path.join(in_dir, files[0]))
+            else:
+                hdr_input = fits.getheader(files[0])
+
+            # Derive shape from header entries
+            if 'NAXIS3' in hdr_input:
                 shape = (hdr_input['NAXIS2'], hdr_input['NAXIS3'])
-            except KeyError:
+            else:
                 shape = (hdr_input['NAXIS1'], hdr_input['NAXIS2'])
 
+        # Initialize as parent class instance
         super().__init__(filename=filename, shape=shape, extensions=None, cards=cards, timestamp=False, path=out_dir,
                          header_card_prefix=header_card_prefix)
