a8ad76bcc7fb40e33bd00a340da5332f2fe5ce33
Felix Bosco <bosco@mpia.de>
2020-09-17 12:33:08 +0200

    Embed new PSFModel class for creating the apodization PSF

--- specklepy/core/apodization.py
+++ specklepy/core/apodization.py
@@ -1,8 +1,6 @@
 import numpy as np
-from astropy.modeling.models import Gaussian2D
-from astropy.modeling.models import AiryDisk2D
 
-from specklepy.exceptions import SpecklepyValueError
+from specklepy.core.psfmodel import PSFModel
 from specklepy.logging import logger
 from specklepy.utils.transferfunctions import otf
 
@@ -29,30 +27,13 @@ def apodize(fourier_object, function='gaussian', crop=False, **kwargs):
         logger.warn("specklepy.core.apodization.apodize received a non quadratic input image. This may cause some "
                     "unpredictable results!")
 
-    # Interpret function input and initialize model function
-    center = ((fourier_object.shape[0] + 1) / 2, (fourier_object.shape[1] + 1) / 2)
-
-    if 'gauss' in function.lower():
-        if 'radius' in kwargs:
-            # Copying the radius keyword argument into the proper Gaussian2D keywords
-            kwargs['x_stddev'] = kwargs['radius']
-            kwargs['y_stddev'] = kwargs['radius']
-            del kwargs['radius']
-        model = Gaussian2D(x_mean=center[0], y_mean=center[1], **kwargs)
-
-    elif 'airy' in function.lower():
-        model = AiryDisk2D(x_0=center[0], y_0=center[1], **kwargs)
-
-    else:
-        raise SpecklepyValueError("func apodize", "function", function, "either gaussian or airy")
-
-    # Evaluate apodization function on object image grid and multiply on object
-    y, x = np.mgrid[0:fourier_object.shape[0], 0:fourier_object.shape[1]]
-    apodization_psf = model(x, y)
+    # Interpret function input and comput apodization PSF
+    psf_model = PSFModel(type=function, **kwargs)
+    apodization_psf = psf_model(fourier_object.shape)
 
     # Crop corners of the PSF
     if crop:
-        threshold = apodization_psf[0, int(center[1])]
+        threshold = apodization_psf[0, int(psf_model.center[1])]
         apodization_psf -= threshold
         apodization_psf = np.maximum(apodization_psf, 0.0)
 
