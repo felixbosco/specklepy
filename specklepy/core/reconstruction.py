b4517129a8920eff0ac28e80af7c2ec24189b992
Felix Bosco <bosco@mpia.de>
2020-09-17 14:32:25 +0200

    Set up structure for initializing the output image

--- specklepy/core/reconstruction.py
+++ specklepy/core/reconstruction.py
@@ -1,3 +1,8 @@
+import numpy as np
+import os
+
+from astropy.io import fits
+
 from specklepy.exceptions import SpecklepyTypeError, SpecklepyValueError
 from specklepy.logging import logger
 
@@ -12,11 +17,12 @@ class Reconstruction(object):
 
     supported_modes = ['full', 'same', 'valid']
 
-    def __init__(self, in_files, mode='full', outfile=None, reference_image=None):
+    def __init__(self, in_files, mode='full', reference_image=None, out_file=None, in_dir=None):
         """Create a Reconstruction instance.
 
         Args:
             in_files (list):
+                List of input data cubes.
             mode (str, optional):
                 Reconstruction mode, defines the final image size and can be `full`, `same` and `valid`. The final image
                 sizes is derived as follows:
@@ -27,10 +33,12 @@ class Reconstruction(object):
                     The reconstruction image covers the same field of view as the image in the reference file.
                 - `valid`:
                     The reconstruction image covers only that field that is covered by all images in the input files.
-            outfile (str, optional):
-                Name of an output file to store the reconstructed image in.
             reference_image (int or str, optional):
                 The index in the `in_files` list or the name of the image serving as reference in 'same' mode.
+            out_file (str, optional):
+                Name of an output file to store the reconstructed image in.
+            in_dir (str, optional):
+                Path to the `in_files`.
         """
 
         # Check input parameter types
@@ -38,8 +46,8 @@ class Reconstruction(object):
             raise SpecklepyTypeError('Reconstruction', 'in_files', type(in_files), 'list')
         if not isinstance(mode, str):
             raise SpecklepyTypeError('Reconstruction', 'mode', type(mode), 'str')
-        if outfile is not None and not isinstance(outfile, str):
-            raise SpecklepyTypeError('Reconstruction', 'outfile', type(outfile), 'str')
+        if out_file is not None and not isinstance(out_file, str):
+            raise SpecklepyTypeError('Reconstruction', 'outfile', type(out_file), 'str')
         if reference_image is not None and not isinstance(reference_image, (int, str)):
             raise SpecklepyTypeError('Reconstruction', 'reference_image', type(reference_image), 'int or str')
 
@@ -50,12 +58,40 @@ class Reconstruction(object):
             logger.warning("Reconstruction requires a reference image if run in `same` mode! The first image will "
                            "be used as reference field.")
             reference_image = 0
+        if in_dir is None:
+            in_dir = ''
 
         # Store input data
         self.in_files = in_files
         self.mode = mode
-        self.outfile = outfile
+        self.outfile = out_file
         self.reference_image = reference_image
 
+        # Derive shape of individual input frames
+        single_cube_mode = len(self.in_files) == 1
+        example_frame = fits.getdata(os.path.join(in_dir, self.in_files[0]))
+        if example_frame.ndim == 3:
+            example_frame = example_frame[0]
+        frame_shape = example_frame[0]
+
         # Initialize image
+        if single_cube_mode:
+            self.image = np.zeros(frame_shape)
+            self.shifts = (0, 0)
+        else:
+            # Estimate relative shifts
+            self.shifts = None
+
+            # Derive corresponding padding vectors
+            pass
+
+            # Derive corresponding image sizes
+            if self.mode == 'same':
+                self.image = np.zeros(frame_shape)
+            elif self.mode == 'full':
+                pass
+            elif self.mode == 'valid':
+                pass
+
+        # Initialize out file
         pass
--- specklepy/tests/test_reconstruction.py
+++ specklepy/tests/test_reconstruction.py
@@ -8,14 +8,14 @@ class TestReconstruction(unittest.TestCase):
         self.in_files = []
 
     def test_init(self):
-        Reconstruction(in_files=[], mode='full', outfile='outfile.fits', reference_image=0)
+        Reconstruction(in_files=[], mode='full', out_file='outfile.fits', reference_image=0)
         with self.assertRaises(TypeError):
             Reconstruction(in_files='')
             Reconstruction(in_files=0)
             Reconstruction(in_files=None)
             Reconstruction(in_files=[], mode=None)
             Reconstruction(in_files=[], mode=0)
-            Reconstruction(in_files=[], outfile=0)
+            Reconstruction(in_files=[], out_file=0)
             Reconstruction(in_files=[], reference_image=0.2)
         with self.assertRaises(ValueError):
             Reconstruction(in_files=[], mode='nonSense')
