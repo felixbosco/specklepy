9ecd9de675cad1a5afe00b7af07bd5ac046396e0
Felix Bosco <felixbosco@gmx.de>
2020-10-07 16:52:31 +0200

    Implement reader for diversely formatted ASCII tables

---
+++ specklepy/io/table.py
@@ -0,0 +1,24 @@
+from astropy.io.registry import IORegistryError
+from astropy.table import Table
+
+
+def read_table(file_name, format=None, error=True):
+
+    # Initialize output table
+    table = None
+
+    # Read table from file
+    if format is not None:
+        table = Table.read(file_name, format=format)
+    else:
+        # Try table default table formats
+        for _format in [None, 'ascii.fixed_width', 'ascii']:
+            try:
+                table = Table.read(file_name, format=_format)
+            except IORegistryError:
+                pass
+
+    if error and table is None:
+        raise RuntimeError(f"Reading the table from {file_name} was not successful!")
+
+    return table
