f7187e8fcb3673998331b4607ae71b77f128694d
Felix Bosco <bosco@mpia.de>
2020-10-21 15:17:30 +0200

    Rewrite PhaseScreen class

--- specklepy/utils/phasescreen.py
+++ specklepy/utils/phasescreen.py
@@ -1,34 +1,28 @@
 import numpy as np
 from scipy.fftpack import fft2, fftshift
 
+
 class PhaseScreen(object):
 
     def __init__(self, k0=3, norm=3, **kwargs):
         self.k0 = k0
         self.norm = norm
 
-    @property
-    def amplitude(self):
-        return self.amp
+    @staticmethod
+    def random_phase(size=256):
+        return np.random.rand(size, size) * 2 * np.pi
 
-    def __call__(self):
-        return self.norm * self.amp * np.exp(1j * self.phase)
+    def amplitude(self, size=256):
+        return np.power(np.square(self.initialize_radii(size=size) + np.square(self.k0)), -11 / 12)
 
-    def make_distance_map(self, size, radius=None):
-        if radius is None:
-            radius = size / 2
+    @staticmethod
+    def initialize_radii(size=256):
+        center = size / 2
         xx, yy = np.mgrid[:size, :size]
-        return np.sqrt(np.square(xx - radius) + np.square(yy - radius))
+        return np.sqrt(np.square(xx - center) + np.square(yy - center))
 
-    def make_aperture(self, size, radius, amplitude=1):
-        return (self.make_distance_map(size=size, radius=radius) <= radius)
+    def psd(self, size=256):
+        return self.norm * self.amplitude(size=size) * np.exp(1j * self.random_phase(size=size))
 
-    def make_phase(self, size):
-        #yy, xx = np.ogrid[-size: size+1, -size: size+1]
-        #kr = np.sqrt(xx**2 + yy**2)
-        kr = self.make_distance_map(size=size)
-        psd_amp = np.power(np.square(kr) + np.square(self.k0), -11 / 12)
-        psd_pha = np.random.rand(size, size) * 2 * np.pi
-        psd = self.norm * psd_amp * np.exp(1j * psd_pha)
-        self.screen = fft2(fftshift(psd)).real
-        return self.screen
+    def generate(self, size=256):
+        return fft2(fftshift(self.psd(size=size))).real
