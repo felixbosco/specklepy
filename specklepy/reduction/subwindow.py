80f5cb07f7321d6e95cb020f01cbd8c0a60c91aa
Felix Bosco <bosco@mpia.de>
2020-11-10 14:25:38 +0100

    SubWindow can now be initialized with a reference for the full window

--- specklepy/reduction/subwindow.py
+++ specklepy/reduction/subwindow.py
@@ -7,17 +7,35 @@ from specklepy.utils.box import Box
 class SubWindow(Box):
 
     @classmethod
-    def from_str(cls, val=None):
+    def from_str(cls, s=None, full=None):
 
         # Create full box window if no string provided
-        if val is None:
+        if s is None:
             return cls(indexes=None)
 
         # Unravel coordinates from string
-        val = val.replace(' ', '')
-        val = val.replace('[', '')
-        val = val.replace(']', '')
-        x_intv, y_intv = val.split(',')
+        indexes = cls.unravel_string(s=s)
+
+        # Provide indexes relative to a full window
+        if full is not None:
+            full = cls.unravel_string(s=full)
+            indexes = indexes - full
+            indexes = np.where(indexes == 0, None, indexes)  # Avoid IndexErrors by substituting the edges by Nones
+
+        return cls(indexes=indexes)
+
+    @staticmethod
+    def unravel_string(s):
+        s = s.replace(' ', '')
+        s = s.replace('[', '')
+        s = s.replace(']', '')
+        y_intv, x_intv = s.split(',')
         x_min, x_max = x_intv.split(':')
         y_min, y_max = y_intv.split(':')
-        return cls(indexes=np.array([x_min, x_max, y_min, y_max]).astype(int))
+        return np.array([x_min, x_max, y_min, y_max]).astype(int)
+
+    # def switch_to_zero_based_indexing(self):
+    #     self.x_min -= 1
+    #     # self.x_max -= 1
+    #     self.y_min -= 1
+    #     # self.y_max -= 1
