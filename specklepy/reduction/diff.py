e0fd9ba8d7067c8e6fd341ef4f9d1775d942838d
Felix Bosco <felixbosco@gmx.de>
2020-11-13 13:04:42 +0100

    Split up function for estimating the mean frame EXPTIME into two

--- specklepy/reduction/diff.py
+++ specklepy/reduction/diff.py
@@ -46,16 +46,51 @@ def differentiate_cube(files, exposure_time_prefix=None, extension=None, dtype=N
 
 
 def estimate_frame_exposure_times(header, common_header_prefix):
+    """Estimate a mean exposure time per frame
 
-    times = []
-    for card in header.cards:
-        if common_header_prefix in card.keyword:
-            times.append(card.value)
-    times.pop(0)
+    Args:
+        header (fits.Header):
+            FITS header to search for the time stamps.
+        common_header_prefix (str):
+            Common prefix among the header keywords storing time stamp information.
 
-    diff_times = np.diff(times)
+    Returns:
+        mean_exposure_time (float):
+            Mean of the exposure times per frame.
+    """
+
+    # Differentiate the time stamp values to obtain time deltas
+    diff_times = np.diff(extract_time_stamps(header, common_header_prefix))
 
     # Report statistics
     logger.info(f"Exposure time is: {np.mean(diff_times):.3f} ({np.std(diff_times):.2e})")
 
     return np.mean(diff_times)
+
+
+def extract_time_stamps(header, common_header_prefix):
+    """Extract the time stamp values from a FITS header.
+
+    Args:
+        header (fits.Header):
+            FITS header to search for the time stamps.
+        common_header_prefix (str):
+            Common prefix among the header keywords storing time stamp information.
+
+    Returns:
+        time_stamps (list):
+            List of time stamp values, typically in units of seconds.
+    """
+
+    # Initialize list
+    time_stamps = []
+
+    # Iterate through header cards
+    for keyword, value in header.items():
+        if common_header_prefix in keyword:
+            time_stamps.append(value)
+
+    # Remove the zero-th entry
+    time_stamps.pop(0)
+
+    return time_stamps
