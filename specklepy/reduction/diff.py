38d21825073ec76098aae222fd832aaea96bc9c2
Felix Bosco <felixbosco@gmx.de>
2020-11-13 18:09:25 +0100

    Implement a cube differentiation using linear regression

--- specklepy/reduction/diff.py
+++ specklepy/reduction/diff.py
@@ -2,13 +2,16 @@ import numpy as np
 import os
 
 from astropy.io import fits
+from astropy.stats import sigma_clip, sigma_clipped_stats
 
 from specklepy.logging import logger
 
 
 def differentiate_cube(files, exposure_time_prefix=None, extension=None, dtype=None, debug=False):
 
-    hdu = 0 if extension is None else extension
+    # Set logging level
+    if debug:
+        logger.setLevel('DEBUG')
 
     # Iterate through files
     for file in files:
@@ -22,8 +25,9 @@ def differentiate_cube(files, exposure_time_prefix=None, extension=None, dtype=N
         with fits.open(diff_file, mode='update') as hdu_list:
 
             # Load input data cube
-            cube = hdu_list[hdu].data
+            cube = hdu_list[extension].data
             if dtype is not None:
+                logger.info(f"Casting data to dtype {dtype!r}")
                 cube = cube.astype(eval(dtype))
 
             # Difference the frames along the time axis
@@ -33,12 +37,78 @@ def differentiate_cube(files, exposure_time_prefix=None, extension=None, dtype=N
 
             # Update exposure time in header
             if exposure_time_prefix is not None:
-                exptime = estimate_frame_exposure_times(hdu_list[hdu].header, exposure_time_prefix)
-                hdu_list[hdu].header.set('FEXPTIME', np.around(exptime, 3), 'Frame exposure time (s)')
+                exptime = estimate_frame_exposure_times(hdu_list[extension].header, exposure_time_prefix)
+                hdu_list[extension].header.set('FEXPTIME', np.around(exptime, 3), 'Frame exposure time (s)')
 
             # Overwriting data
             logger.info("Storing data to file...")
-            hdu_list[hdu].data = cube
+            hdu_list[extension].data = cube
+            hdu_list.flush()
+
+        # Final terminal output
+        logger.info(f"Differencing successful for file {diff_file}")
+
+
+def differentiate_linear_reg(files, exposure_time_prefix=None, extension=None, dtype=None, debug=False):
+
+    # Set defaults
+    slope_mask_sigma = 10
+    degree = 1
+
+    # Set logging level
+    if debug:
+        logger.setLevel('DEBUG')
+
+    # Iterate through files
+    for file in files:
+
+        # Make a new copy of the file
+        diff_file = 'diff_' + os.path.basename(file)
+        logger.info(f"Creating file {diff_file}")
+        os.system(f"cp {file} {diff_file}")
+
+        # Load original data and difference
+        with fits.open(diff_file, mode='update') as hdu_list:
+
+            # Load input data cube
+            header = hdu_list[extension].header
+            cube = hdu_list[extension].data
+            if dtype is not None:
+                logger.info(f"Casting data to dtype {dtype!r}")
+                cube = cube.astype(eval(dtype))
+
+            # Update exposure time in header
+            if exposure_time_prefix is not None:
+                exptime = estimate_frame_exposure_times(header, exposure_time_prefix)
+                hdu_list[extension].header.set('FEXPTIME', np.around(exptime, 3), 'Frame exposure time (s)')
+
+            # Initialize arrays
+            x = extract_time_stamps(header=header, common_header_prefix=exposure_time_prefix)
+            x = np.subtract(x, x[0])
+            slopes = np.empty(cube[0].shape)
+            # slopes_var = np.empty(cube[0].shape)
+            intercepts = np.empty(cube[0].shape)
+            # intercepts_var = np.empty(cube[0].shape)
+
+            # Linear regression
+            for row in range(cube.shape[1]):
+                for col in range(cube.shape[2]):
+                    y = cube[:, row, col]
+                    coefficients = np.polyfit(x, y, deg=degree)
+                    # coefficients, cov = np.polyfit(x, y, deg=degree, cov=True)
+                    slopes[row, col] = coefficients[-2]
+                    # slopes_var[row, col] = cov[-2, -2]
+                    intercepts[row, col] = coefficients[-1]
+                    # intercepts_var[row, col] = cov[-1, -1]
+
+            # Create mask
+            mask = sigma_clip(slopes, sigma=slope_mask_sigma).mask
+            mask_hdu = fits.ImageHDU(data=mask, name='MASK')
+
+            # Overwriting data
+            logger.info("Storing data to file...")
+            hdu_list[extension].data = cube
+            hdu_list.append(mask_hdu)
             hdu_list.flush()
 
         # Final terminal output
