938d7ed476969fedac5f9eb2ab965b80ce15eb1e
Felix Bosco <bosco@mpia.de>
2020-11-26 16:45:39 +0100

    Start implementing a DataReduction class

---
+++ specklepy/reduction/datareduction.py
@@ -0,0 +1,38 @@
+import os
+
+from specklepy.logging import logger
+
+
+class DataReduction(object):
+
+    def __init__(self, **kwargs):
+        self.paths = kwargs.get('PATHS')
+        self.flat_fielding = kwargs.get('FLAT')
+        self.sky_subtraction = kwargs.get('SKY')
+        self.options = kwargs.get('OPTIONS')
+
+    def run(self):
+        self.initialize_directories()
+        self.run_flat_fielding()
+        self.run_linearization()
+        self.run_sky_subtraction()
+
+        # Close reduction
+        logger.info("Reduction finished!")
+
+    def initialize_directories(self):
+        if not os.path.isdir(self.paths.get('outDir')):
+            logger.debug(f"Making directory {self.paths.get('outDir')}")
+            os.makedirs(self.paths.get('outDir'))
+        if not os.path.isdir(self.paths.get('tmpDir')):
+            logger.debug(f"Making directory {self.paths.get('tmpDir')}")
+            os.makedirs(self.paths.get('tmpDir'))
+
+    def run_flat_fielding(self):
+        pass
+
+    def run_linearization(self):
+        pass
+
+    def run_sky_subtraction(self):
+        pass
