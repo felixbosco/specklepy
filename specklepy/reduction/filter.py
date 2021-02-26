f9d3e692566d56e41237ceaf864fc66ba75e5dff
Felix Bosco <felixbosco@gmx.de>
2021-02-26 16:25:18 +0100

    Implement function for masking hot pixels

---
+++ specklepy/reduction/filter.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def mask_hot_pixels(image, sigma_threshold=5):
+    # Compute second derivatives
+    ddx = np.diff(image, n=2)
+    ddy = np.diff(image, n=2, axis=-2)
+
+    # Pad derivatives to return to image size
+    ddx = np.pad(ddx, ((0, 0), (1, 1)))
+    ddy = np.pad(ddy, ((1, 1), (0, 0)))
+
+    # Derive masks
+    mask_x = ddx < -sigma_threshold * np.std(ddx)
+    mask_y = ddy < -sigma_threshold * np.std(ddy)
+
+    return np.ma.masked_array(image, mask=mask_x | mask_y)
+
+
+def hot_pixel_mask(image, sigma_threshold=5):
+    return mask_hot_pixels(image=image, sigma_threshold=sigma_threshold).mask
