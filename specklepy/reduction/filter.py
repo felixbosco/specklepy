1ebd7244d5833939e48ae9d1203add5aafa141d5
Felix Bosco <felixbosco@gmx.de>
2021-03-12 17:28:15 +0100

    Create functions for identifying, masking, and filling hot and variable pixels

--- specklepy/reduction/filter.py
+++ specklepy/reduction/filter.py
@@ -1,7 +1,20 @@
 import numpy as np
 
 
-def mask_hot_pixels(image, sigma_threshold=3):
+def hot_pixel_mask(image, threshold=5):
+    """Identify hot pixels via a 2nd derivative method and create a hot pixel mask with hot pixels `True`.
+
+    Arguments:
+        image (np.ndarray):
+            Image for which the mask shall be created.
+        threshold (int or float, optional):
+            Sigma-clipping threshold, used to evaluate whether the 2nd derivative is too large.
+
+    Returns:
+        mask (np.ndarray):
+            Boolean array of the shape of the image input. Hot pixels are `True`.
+    """
+
     # Compute second derivatives
     ddx = np.diff(image, n=2)
     ddy = np.diff(image, n=2, axis=-2)
@@ -11,11 +24,29 @@ def mask_hot_pixels(image, sigma_threshold=3):
     ddy = np.pad(ddy, ((1, 1), (0, 0)))
 
     # Derive masks
-    mask_x = np.abs(ddx) > sigma_threshold * np.std(ddx)
-    mask_y = np.abs(ddy) > sigma_threshold * np.std(ddy)
+    mask_x = np.abs(ddx) > threshold * np.std(ddx)
+    mask_y = np.abs(ddy) > threshold * np.std(ddy)
 
-    return np.ma.masked_array(image, mask=mask_x | mask_y)
+    return mask_x | mask_y
 
 
-def hot_pixel_mask(image, sigma_threshold=5):
-    return mask_hot_pixels(image=image, sigma_threshold=sigma_threshold).mask
+def mask_hot_pixels(image, threshold=5):
+    return np.ma.masked_array(image, mask=hot_pixel_mask(image=image, threshold=threshold))
+
+
+def fill_hot_pixels(image, fill_value=0, threshold=5):
+    return mask_hot_pixels(image=image, threshold=threshold).filled(fill_value)
+
+
+def variable_pixel_mask(cube, var, threshold=5):
+    std = np.std(cube, axis=0)
+    return np.divide(std, np.sqrt(var)) > threshold
+
+
+def mask_variable_pixels(cube, var, threshold=5):
+    integrated = np.sum(cube, axis=0)
+    return np.ma.masked_array(integrated, mask=variable_pixel_mask(cube=cube, var=var, threshold=threshold))
+
+
+def fill_variable_pixels(cube, var, fill_value=0, threshold=5):
+    return mask_variable_pixels(cube=cube, var=var, threshold=threshold).filled(fill_value)
