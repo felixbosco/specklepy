a511c6468b167bc17040bd4b9a1969143ec9a61f
Felix Bosco <felixbosco@gmx.de>
2021-03-15 13:34:52 +0100

    Change order of coordinate shifts

--- specklepy/core/alignment.py
+++ specklepy/core/alignment.py
@@ -167,7 +167,7 @@ class ShiftEstimator(object):
 
                     # Derive shift
                     # shift = int(round(reference_position[1] - pos[1])), int(round(reference_position[0] - pos[0]))
-                    shift = int(round(reference_position[0] - pos[0])), int(round(reference_position[1] - pos[1]))
+                    shift = int(round(pos[0] - reference_position[0])), int(round(pos[1] - reference_position[1]))
 
                 logger.info(f"Estimated shift {shift} for file {file!r}")
                 self.shifts.append(shift)
