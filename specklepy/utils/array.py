44c9283329492feeb0c1da7e2a9e695ab13c6eac
Felix Bosco <felixbosco@gmx.de>
2021-03-18 13:03:17 +0100

    Implement frame number extraction

--- specklepy/utils/array.py
+++ specklepy/utils/array.py
@@ -1,5 +1,23 @@
-import numpy as np
-
-
 def frame_shape(array):
     return array.shape[-2], array.shape[-1]
+
+
+def frame_number(array):
+    """Extract the number of frames in an array.
+
+    Arguments:
+        array (array-like):
+            Array to extract the frame number from.
+
+    Returns:
+        frame_number (int):
+            Number of frames in the array
+    """
+    if array.ndim == 2:
+        number_frames = 1
+    elif array.ndim == 3:
+        number_frames = array.shape[0]
+    else:
+        raise ValueError(f"Data is {array.ndim}-dimensional but must be 2 or 3-dimensional!")
+
+    return number_frames
