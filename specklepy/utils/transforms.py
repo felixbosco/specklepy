1ba875f621354495cd47a21b22d2e7a37ed9a428
Felix Bosco <felixbosco@gmx.de>
2021-04-01 13:02:44 +0200

    Implement transformations between angular and pixel coordinates

--- specklepy/utils/transforms.py
+++ specklepy/utils/transforms.py
@@ -9,3 +9,13 @@ def rad2deg(val):
 def rad2mas(val):
     """Transform an angle from units of radians into milli-arcseconds."""
     return np.rad2deg(val) * 60**2 * 1000
+
+
+def ang2pix(val, pixel_scale):
+    """Transform an angle from angular units into pixels."""
+    return val / pixel_scale
+
+
+def pix2ang(val, pixel_scale):
+    """Transform an angle from units of pixels into angular units."""
+    return val * pixel_scale
