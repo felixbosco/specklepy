73a41234813d3ad30b0212a1f1620ea949f8c6ec
Felix Bosco <felixbosco@gmx.de>
2021-04-06 14:59:33 +0200

    Add documentation

--- specklepy/io/fits.py
+++ specklepy/io/fits.py
@@ -5,6 +5,28 @@ from astropy.io import fits
 
 
 def get_data(file_name, path=None, extension=None, squeeze=True, dtype=None, ignore_missing_extension=False):
+    """Read data from a FITS file.
+
+    Args:
+        file_name (str):
+            Name of the file to read data from.
+        path (str, optional):
+            The path will be joined with `file_name`, if provided.
+        extension (str, optional):
+            Name of a FITS file extension. The behavior for missing extensions depends on the
+            `ignore_missing_extension` argument.
+        squeeze (bool, optional):
+            If requested, the data array will be squeezed, i.e. length-1 dimensions are dropped.
+        dtype (data type, optional):
+            Data will be casted to this data type, if provided.
+        ignore_missing_extension (bool, optional):
+            Set the behavior for missing extensions. If `False`, a KeyError is raised, and otherwise the function
+            returns a `None`.
+
+    Returns:
+        data (np.ndarray or None-type):
+            Data array from the requested FITS file extension. Can return `None`, if extension is not contained.
+    """
 
     # Construct path
     if path is None:
