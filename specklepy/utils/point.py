b615e2b93b83dca2d0cb7d00209a0dd4fede34cf
Felix Bosco <felixbosco@gmx.de>
2021-04-09 19:07:04 +0200

    Implement basic vector algebra for Point class

--- specklepy/utils/point.py
+++ specklepy/utils/point.py
@@ -1,10 +1,13 @@
+import numpy as np
+
+
 class Point(object):
 
     def __init__(self, *args, order='xy'):
 
-        self.x = None
-        self.y = None
-        self.z = None
+        self.x = 0
+        self.y = 0
+        self.z = 0
         self.order = order
 
         for a, arg in enumerate(args):
@@ -16,3 +19,21 @@ class Point(object):
     @property
     def ndim(self):
         return len(self.order)
+
+    def __add__(self, other):
+        dx = self.x + other.x
+        dy = self.y + other.y
+        dz = self.z + other.z
+        return Point(dx, dy, dz, order='xyz')
+
+    def __sub__(self, other):
+        dx = self.x - other.x
+        dy = self.y - other.y
+        dz = self.z - other.z
+        return Point(dx, dy, dz, order='xyz')
+
+    def __len__(self):
+        return np.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    def distance_to(self, other):
+        return len(self - other)
