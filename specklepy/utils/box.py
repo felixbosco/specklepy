c86410811e59e3adb6276cf499d5cfbfe801b212
Felix Bosco <felixbosco@gmx.de>
2021-04-09 19:57:33 +0200

    Implement method for casting indexes to integer values, called after init and shift

--- specklepy/utils/box.py
+++ specklepy/utils/box.py
@@ -52,6 +52,16 @@ class Box(object):
                     self.y_min = indexes[2]
                     self.y_max = indexes[3]
 
+        # Assert integer indexes
+        self.round_indexes()
+
+    def round_indexes(self):
+        for attr in ['x_min', 'x_max', 'y_min', 'y_max']:
+            try:
+                self.__setattr__(attr, int(round(self.__getattribute__(attr))))
+            except TypeError:
+                pass
+
     def __repr__(self):
         return f"[[{self.x_min}, {self.x_max}], [{self.y_min}, {self.y_max}]]"
 
@@ -60,31 +70,9 @@ class Box(object):
         # Abort if Box is undefined
         if self.x_min is None and self.x_max is None and self.y_min is None and self.y_max is None:
 
-            # # Update limits
-            # if update:
-            #     self.x_min = 0
-            #     self.x_max = array.shape[1] - 1
-            #     self.y_min = 0
-            #     self.y_max = array.shape[0] - 1
-
             return array
 
         else:
-            # # Set limits
-            # x_min = 0 if self.x_min is None else self.x_min
-            # x_max = array.shape[1] - 1 if self.x_max is None else self.x_max
-            # y_min = 0 if self.y_min is None else self.y_min
-            # y_max = array.shape[0] - 1 if self.y_max is None else self.y_max
-            #
-            # # Update limits
-            # if update:
-            #     self.x_min = x_min
-            #     self.x_max = x_max
-            #     self.y_min = y_min
-            #     self.y_max = y_max
-            #
-            # # Create index sets
-            # x, y = np.meshgrid(range(self.y_min, self.y_max), range(self.x_min, self.x_max))
 
             return array[self.y_min: self.y_max, self.x_min: self.x_max]
 
@@ -115,6 +103,8 @@ class Box(object):
             obj.x_max += shift[1]
             obj.y_min += shift[0]
             obj.y_max += shift[0]
+
+        obj.round_indexes()
 
         return obj
 
