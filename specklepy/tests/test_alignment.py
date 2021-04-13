6d1e88c172768d2dc868d93e283fe55693aaf07c
Felix Bosco <felixbosco@gmx.de>
2021-04-13 14:12:40 +0200

    Remove deprecated functions that have been replaced by class methods

--- specklepy/tests/test_alignment.py
+++ specklepy/tests/test_alignment.py
@@ -1,7 +1,6 @@
-import unittest
-import os
 import numpy as np
-from astropy.io import fits
+import os
+import unittest
 
 from specklepy.io.filearchive import FileArchive
 from specklepy.core import alignment
@@ -12,23 +11,10 @@ class TestAlignment(unittest.TestCase):
 
     def setUp(self):
         self.path = 'specklepy/tests/files/'
-        self.files = FileArchive(os.path.join(self.path, 'synthetic/glao_600ms*.fits')).files
+        self.files = FileArchive('synthetic/glao_600ms*.fits', in_dir=self.path).files
         self.shifts = [(0, 0), (34, -20), (-14, -51)]
         self.image_shape = (512, 512)
         self.cube_shape = (10, 512, 512)
-
-    def test_get_shift(self):
-        image = fits.getdata(self.path + 'tmp/glao_200ms_1_ssa.fits')
-        ref_image = fits.getdata(self.path + 'tmp/glao_200ms_2_ssa.fits')
-        shift_peak = alignment.estimate_shift(image=image, reference_image=ref_image, mode='peak')
-        shift_max  = alignment.estimate_shift(image=image, reference_image=ref_image, mode='maximum')
-        shift_corr = alignment.estimate_shift(image=image, reference_image=ref_image, mode='correlation')
-        self.assertEqual(shift_max, shift_peak)
-        self.assertAlmostEqual(shift_max, shift_corr)
-
-    def test_get_shifts(self):
-        # alignment.get_shifts(self.files, mode='peak')
-        alignment.estimate_shifts(self.files, mode='correlation')
 
     def test_get_pad_vectors(self):
         alignment.derive_pad_vectors(self.shifts, cube_mode=(len(self.cube_shape) == 3))
@@ -40,7 +26,6 @@ class TestAlignment(unittest.TestCase):
         pad_vectors, ref_pad_vector = alignment.derive_pad_vectors(self.shifts, cube_mode=True)
         for pad_vector in pad_vectors:
             padded = alignment.pad_array(np.ones(self.cube_shape), pad_vector=pad_vector, mode='same', reference_image_pad_vector=ref_pad_vector)
-
 
 
 if __name__ == "__main__":
