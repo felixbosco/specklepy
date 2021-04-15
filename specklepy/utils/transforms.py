732a76895972e643a208372258c797ba3aa7ff12
Felix Bosco <felixbosco@gmx.de>
2021-04-15 16:18:16 +0200

    Implement transformations between magnitudes and fluxes

--- specklepy/utils/transforms.py
+++ specklepy/utils/transforms.py
@@ -7,7 +7,7 @@ def rad2deg(val):
 
 
 def rad2mas(val):
-    """Transform an angle from units of radians into milli-arcseconds."""
+    """Transform an angle from units of radians into milliarcseconds."""
     return np.rad2deg(val) * 60**2 * 1000
 
 
@@ -19,3 +19,13 @@ def ang2pix(val, pixel_scale):
 def pix2ang(val, pixel_scale):
     """Transform an angle from units of pixels into angular units."""
     return val * pixel_scale
+
+
+def flux2mag(flux, flux_reference):
+    """Transform a brightness value from units of flux into magnitudes."""
+    return -2.5 * np.log10(np.divide(flux, flux_reference))
+
+
+def mag2flux(mag, flux_reference=1):
+    """Transform a brightness value from units of magnitudes into a flux, in the units of the reference flux."""
+    return np.power(10, np.divide(mag, -2.5)) * flux_reference
