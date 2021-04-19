8aeae76c6e4903b663ee505e7e425f4fe2f54cca
Felix Bosco <felixbosco@gmx.de>
2021-04-19 16:37:20 +0200

    Implement basic transformation features for PhotometricSystem

---
+++ specklepy/mock/photometricsystem.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+
+from astropy.constants import h as PLANCK
+from astropy.table import Table
+from astropy.units import Unit, Quantity
+
+
+class PhotometricSystem(object):
+
+    def __init__(self, file_name='default'):
+
+        # Store file name
+        if file_name == 'default':
+            defaults_file = os.path.join(os.path.dirname(__file__), '../data/photometry.fits')
+            self.data_file = os.path.abspath(defaults_file)
+        else:
+            self.data_file = file_name
+
+        # Initialize attributes
+        self._band = None
+        self._reference_photon_flux = None
+
+    def reference_photon_flux(self, band=None):
+        if (band is None or band == self._band) and self._reference_photon_flux is not None:
+            pass
+        else:
+            self._reference_photon_flux = self.read_reference_photon_flux(band=band)
+        return self._reference_photon_flux
+
+    def read_reference_photon_flux(self, band, **kwargs):
+        table = Table.read(self.data_file, **kwargs)
+        row_index = np.where(table["Band"] == band)
+        fwhm = table['FWHM'][row_index][0]
+        flux = table['Flux'][row_index][0] * Unit('Jy')
+        return (flux / PLANCK * fwhm * Unit('photon')).decompose()
+
+    def to_photon_flux(self, magnitudes, band=None):
+        if isinstance(magnitudes, Quantity):
+            if magnitudes.unit == 'mag':
+                magnitudes = magnitudes.value
+
+        return 10**(magnitudes/-2.5) * self.reference_photon_flux(band=band)
