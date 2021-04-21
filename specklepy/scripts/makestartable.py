aebd19575de2d00e4d12d82ccafb1240026346ba
Felix Bosco <felixbosco@gmx.de>
2021-04-21 15:45:37 +0200

    Add documentation to make_star_table function

--- specklepy/scripts/makestartable.py
+++ specklepy/scripts/makestartable.py
@@ -5,7 +5,39 @@ from astropy.units import Quantity
 from specklepy.mock.startable import StarTable
 
 
-def make_star_table(number_stars, iso_files, lf_files, lf_band, out_file, half_light_radius, population_weights=None):
+def make_star_table(number_stars, iso_files, lf_files, lf_band, half_light_radius, population_weights=None,
+                    out_file=None, table_format=None, overwrite=False):
+    """Create a table of stars and write it to file
+
+    Args:
+        number_stars (int):
+            Total number of stars to create.
+        iso_files (list):
+            List of isochrone files. Must at least contain one file name.
+        lf_files (list):
+            List of luminosity function files. Must contain the same number of file names as `iso_files`.
+        lf_band (str):
+            Name of the filter band that is represented in the luminosity function files. This is used for connecting
+            the corresoonding tables. See documentation of `StarTable.generate_table` method for details.
+        half_light_radius (int or float or str or Quantity):
+            Half light radius of the generated cluster in linear units. Str-type values will be evaluated as a Quantity.
+            Such input can be used to parse different units than parsecs.
+        population_weights (list, optional):
+            List of population weights, i.e. fraction of `number_stars` that is generated for the corresponding
+            population, defined by the isochrone and luminosity function files. The weights are automatically normalized
+            to unity. If not provided, all populations share the equal fraction of `number_stars`.
+        out_file (str, optional):
+            Name of a file, to which the star table shall be written.
+        table_format (str, optional):
+            Format string for writing the table to ASCII files. Falls back to `'ascii.fixed_width'` if not provided or
+            to `'fits'` if `out_file` has the extension '.fits'.
+        overwrite (bool, optional):
+            Allow to overwrite existing table files.
+
+    Returns:
+        star_table (StarTable):
+            StarTable instance that was created based on the provided isochrone and luminosity function files.
+    """
 
     # Assert that the number of isochrone and luminosity function files are equal
     number_populations = len(iso_files)
@@ -17,11 +49,8 @@ def make_star_table(number_stars, iso_files, lf_files, lf_band, out_file, half_l
     if population_weights is None:
         population_weights = np.ones(number_populations)
     population_weights /= np.sum(population_weights)
-    population_sizes = np.empty(number_populations, dtype=int)
-    for w, weight in enumerate(population_weights):
-        population_sizes[w] = int(weight * number_stars)
-    while np.sum(population_sizes) < number_stars:
-        population_sizes[np.argmax(population_weights)] += 1
+    population_sizes_cum = np.cumsum(population_weights * number_stars).round().astype(int)
+    population_sizes = np.append(population_sizes_cum[:1], np.diff(population_sizes_cum))
 
     # Initialize star table and generate the stellar populations
     star_table = StarTable()
@@ -38,4 +67,9 @@ def make_star_table(number_stars, iso_files, lf_files, lf_band, out_file, half_l
     star_table.add_spatial_columns(half_light_radius, half_light_radius_unit)
 
     # Write result to file
-    star_table.write(out_file)
+    if out_file is not None:
+        if not out_file.endswith('.fits') and table_format is None:
+            table_format = 'ascii.fixed_width'
+        star_table.write(out_file, table_format=table_format, overwrite=overwrite)
+
+    return star_table
